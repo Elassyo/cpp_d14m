b4c464d19c4b5d9a11d16ca510c6aa4ce642f5e0
Arthur Melin <arthur.melin@epitech.eu>
2018-01-16 09:29:20 +0100

    00

---
+++ ex00/Lemon.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2018
+** CPP Pool Day 14
+** File description:
+** Lemon class implementation
+*/
+
+#include "Lemon.hpp"
+
+Lemon::Lemon() :
+	Fruit("lemon", 3)
+{
+}
---
+++ ex00/Lemon.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2018
+** CPP Pool Day 14
+** File description:
+** Lemon class definition
+*/
+
+#if !defined (LEMON_HPP_)
+	#define LEMON_HPP_
+
+	#include "Fruit.hpp"
+
+class Lemon : public Fruit
+{
+public:
+	Lemon();
+};
+
+#endif /* !defined (LEMON_HPP_) */
